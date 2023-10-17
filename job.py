24bcdff42e538c6770eeb99772384fc9e4e351f4
Waz <warwickboote@hotmail.com>
2023-10-17 15:02:21 +1100

    Update job.py

--- job.py
+++ job.py
@@ -2,7 +2,7 @@ from autogen import AssistantAgent, UserProxyAgent, config_list_from_json, Group
 
 # Load the configuration for GPT-4 from a JSON file
 config_list_gpt4 = config_list_from_json(
-    "../OAI_CONFIG_LIST.json",
+    "./OAI_CONFIG_LIST.json",
     filter_dict={
         "model": ["gpt-4-0613", "gpt-4-32k", "gpt-4", "gpt-4-0314"],
     },
